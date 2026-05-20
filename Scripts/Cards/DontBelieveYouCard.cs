5ab732cde51218ac82478feeedd33b134027cd74
zhangcm <wxtest15913913929@163.com>
2026-05-20 23:37:11 +0800

    首次成功运行

---
+++ Scripts/Cards/DontBelieveYouCard.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using STS2RitsuLib.Cards.DynamicVars;
+using STS2RitsuLib.Interop.AutoRegistration;
+using STS2RitsuLib.Scaffolding.Content;
+
+namespace DontBelieveYou.Scripts.Cards;
+
+[RegisterCard(typeof(ColorlessCardPool))]
+public class DontBelieveYouCard : ModCardTemplate
+{
+    private const int energyCost = 2;
+    private const CardType type = CardType.Skill;
+    private const CardRarity rarity = CardRarity.Uncommon;
+    private const TargetType targetType = TargetType.AnyAlly;
+    private const bool shouldShowInCardLibrary = true;
+
+    // 卡图资源
+    public override CardAssetProfile AssetProfile => new(
+        PortraitPath: $"res://dont-believe-you/images/cards/{GetType().Name}.png"
+    );
+
+    // 卡牌基础数值 - 能量减少量
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        ModCardVars.Int("EnergyLoss", 2)
+    ];
+
+    public DontBelieveYouCard() : base(energyCost, type, rarity, targetType, shouldShowInCardLibrary)
+    {
+    }
+
+    // 打出时的效果逻辑 - 使队友失去费用
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        // 获取能量减少量
+        int energyLoss = DynamicVars["EnergyLoss"].IntValue;
+
+        // 获取目标玩家（队友）
+        Player? targetPlayer = cardPlay.Target?.Player;
+
+        // 如果目标是玩家，使其失去能量
+        if (targetPlayer != null)
+        {
+            await PlayerCmd.LoseEnergy(energyLoss, targetPlayer);
+        }
+    }
+
+    // 升级后的效果逻辑
+    protected override void OnUpgrade()
+    {
+        // 升级后能量减少量从2变为3
+        DynamicVars["EnergyLoss"].UpgradeValueBy(1);
+    }
+}
---
+++ Scripts/Entry.cs
@@ -0,0 +1,26 @@
+using System.Reflection;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Modding;
+using STS2RitsuLib;
+using STS2RitsuLib.Interop;
+
+namespace DontBelieveYou.Scripts;
+
+[ModInitializer(nameof(Init))]
+public class Entry
+{
+    // 你的modid
+    public const string ModId = "dont-believe-you";
+    public static readonly Logger Logger = RitsuLibFramework.CreateLogger(ModId);
+
+    public static void Init()
+    {
+        // harmony可用，但是最好用ritsu的封装patch，见补丁系统一章
+        // var harmony = new Harmony("com.example.testmod");
+        // harmony.PatchAll();
+        var assembly = Assembly.GetExecutingAssembly();
+        RitsuLibFramework.EnsureGodotScriptsRegistered(assembly, Logger);
+        // 自动注册内容
+        ModTypeDiscoveryHub.RegisterModAssembly(ModId, assembly);
+    }
+}
